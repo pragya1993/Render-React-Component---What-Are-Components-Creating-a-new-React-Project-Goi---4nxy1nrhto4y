643f5c116a95843db4e8c64524ff850259b81962
Pragya <1993pragyapandey@gamil.com>
2022-11-28 20:07:20 +0530

    git add

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,11 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+  const p1 = <p>I am learning React. My life is getting better. </p>
   return (
-    <div id="main"></div>
+    <div id="main">
+      { p1 }
+    </div>
   )
 }
 
